38074d7a63ccbc6dd95e75432df516dae6fe185d
Daniel Allenby <dallenby@ford.com>
2019-06-28 14:27:42 +0100

    Displaying all images

--- src/my-component.js
+++ src/my-component.js
@@ -11,6 +11,7 @@ class BasicSetup extends LitElement {
         background-size: 200px 100px;
         width: 200px;
         height: 100px;
+        float: left;
       }
       #image2 {
         background-image: url("/assets/image2.jpg");
@@ -18,6 +19,58 @@ class BasicSetup extends LitElement {
         width: 200px;
         height: 100px;
         color: white;
+        float: left;
+      }
+      #image3 {
+        background-image: url("/assets/image3.jpg");
+        background-size: 200px 100px;
+        width: 200px;
+        height: 100px;
+        color: white;
+        float: left;
+      }
+      #image4 {
+        background-image: url("/assets/image4.jpg");
+        background-size: 200px 100px;
+        width: 200px;
+        height: 100px;
+        color: white;
+        float: left;
+      }
+      #image5 {
+        background-image: url("/assets/image5.jpg");
+        background-size: 200px 100px;
+        width: 200px;
+        height: 100px;
+        color: white;
+        float: left;
+      }
+      #image6 {
+        background-image: url("/assets/image6.jpg");
+        background-size: 200px 100px;
+        width: 200px;
+        height: 100px;
+        color: white;
+        float: left;
+      }
+      #image7 {
+        background-image: url("/assets/image7.jpg");
+        background-size: 200px 100px;
+        width: 200px;
+        height: 100px;
+        color: white;
+        float: left;
+      }
+      #image8 {
+        background-image: url("/assets/image8.jpg");
+        background-size: 200px 100px;
+        width: 200px;
+        height: 100px;
+        color: white;
+        float: left;
+      }
+      #magnify {
+        height: 400px;
       }
     `;
   }
@@ -26,12 +79,33 @@ class BasicSetup extends LitElement {
     // Return the template using the html template tag. lit-html will parse the template and
     // create the DOM elements
     return html`
-      <div id="image1">
-        <p>Image 1</p>
-      </div>
-      <div id="image2">
-        <p>Image 2</p>
-      </div>
+    <div id="magnify">
+      magnified
+    </div>
+    <div id="image1">
+      <p>Image 1</p>
+    </div>
+    <div id="image2">
+      <p>Image 2</p>
+    </div>
+    <div id="image3">
+      <p>Image 3</p>
+    </div>
+    <div id="image4">
+      <p>Image 4</p>
+    </div>
+    <div id="image5">
+      <p>Image 5</p>
+    </div>
+    <div id="image6">
+      <p>Image 6</p>
+    </div>
+    <div id="image7">
+      <p>Image 7</p>
+    </div>
+    <div id="image8">
+      <p>Image 8</p>
+    </div> 
     `;
   }
 }
